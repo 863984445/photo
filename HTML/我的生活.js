aca67d9470c48d03797ce17a8e644b3ee6246ddb
863984445 <863984445.com>
2020-04-14 22:44:26 +0800

    HTML

---
+++ HTML/我的生活.js
@@ -0,0 +1,4 @@
+function button():{
+	window.open("登入页面.html")
+
+}
---
+++ HTML/登入页面.js
@@ -0,0 +1,7 @@
+function checkpassword(){
+var pwd="712630";//你和朋友约定的密码，写死的
+var inpwd=document.getElementById("password").value;//获取输入的代码
+if(inpwd == pwd){ //判断一下是否一样
+window.location.href="HTML/主页.html";
+}
+}
